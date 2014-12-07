ab4995563f80a5e335e4e70891b5590d64ee49a9
Jacy Wang <jacywj@gmail.com>
2014-12-07 13:36:27 -0500

    Set up Posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,28 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all 
+  end
 
+  def show
+    @post = Post.find(params[:id])
+    if @post.nil?
+      redirect_to :index
+    end
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
   end
 end
