719d57ebe16ea62decd29068ad08393bacaaa642
Jacy Wang <jacywj@gmail.com>
2014-12-24 17:33:53 -0500

    Post slug

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   before_action :require_user
   
   def create 
-    @post = Post.find(params[:post_id])
+    @post = Post.find_by(slug: params[:post_id])
     @comment = @post.comments.new(params.require(:comment).permit(:body))
     @comment.creator = current_user
     if @comment.save
@@ -16,11 +16,17 @@ class CommentsController < ApplicationController
   def vote
     @comment = Comment.find(params[:comment_id])
     @vote = Vote.create(voteable: @comment, creator: current_user, vote: params[:vote])
-    if @vote.valid?
-      flash[:notice] = "Your vote was counted."
-    else
-      flash[:error] = "Your can only vote once for this comment."
+    
+    respond_to do |format|
+      format.html do
+        if @vote.valid?
+          flash[:notice] = "Your vote was counted."
+        else
+          flash[:error] = "Your can only vote once for this comment."
+        end
+        redirect_to :back
+      end
+      format.js
     end
-    redirect_to :back
   end
 end
