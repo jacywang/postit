abe3c790cc133ab9be9b096ab10a41fe66fa27af
Jacy Wang <jacywj@gmail.com>
2014-12-26 18:13:37 -0500

    Voteable module

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,6 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :post_categories
   has_many :categories, through: :post_categories
-  has_many :votes, as: :voteable
 
   validates :title, presence: true, length: {minimum: 5}
   validates :url, presence: true, uniqueness: true
@@ -11,17 +10,7 @@ class Post < ActiveRecord::Base
 
   before_save :generate_slug
 
-  def total_votes
-    up_votes - down_votes
-  end
-
-  def up_votes
-    self.votes.where(vote: true).size
-  end
-
-  def down_votes
-    self.votes.where(vote: false).size
-  end
+  include Voteable
 
   def to_param
     self.slug
